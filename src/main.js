852cdd56535e3bcbb20dfeadddd400666b03e5da
root <99263711@qq.com>
2021-05-13 19:06:40 +0800

    abc

--- src/main.js
+++ src/main.js
@@ -2,6 +2,34 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+// import ECharts from "vue-echarts";
+// import { use } from "echarts/core"
+
+// 手动引入ECharts各模块来减小打包体积
+
+// import {
+//   CanvasRenderer
+// } from 'echarts/renderers'
+// import {
+//   BarChart
+// } from 'echarts/charts'
+// import {
+//   GridComponent,
+//   TooltipComponent
+// } from 'echarts/components'
+
+// use([
+//   CanvasRenderer,
+//   BarChart,
+//   GridComponent,
+//   TooltipComponent
+// ]);
+
+// 全局注册组件（也可以使用局部注册）
+// Vue.component('v-chart', ECharts)
+
+// 整个引入
+import "echarts";
 
 Vue.config.productionTip = false;
 
@@ -10,3 +38,4 @@ new Vue({
   store,
   render: (h) => h(App),
 }).$mount("#app");
+
--- src/router/index.js
+++ src/router/index.js
@@ -19,6 +19,12 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
+  {
+    path: "/demo",
+    name: "Demo",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Demo.vue"),
+  },
 ];
 
 const router = new VueRouter({
